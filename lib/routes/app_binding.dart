d112bf4ac0cdcc4f1f0cb977342f66ceb73aa47f
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2025-10-24 22:00:59 +0700

    fix refresh

--- lib/routes/app_binding.dart
+++ lib/routes/app_binding.dart
@@ -1,40 +1,69 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
-import '../data/interceptors/auth_token_interceptor.dart';
 import '../data/repositories/auth_repository.dart';
+import 'app_routes.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put<Dio>(
-      Dio(
-        BaseOptions(
-          baseUrl: 'http://13.228.127.252:8114',
-          connectTimeout: const Duration(seconds: 10),
-          receiveTimeout: const Duration(seconds: 10),
-          headers: const {
-            Headers.contentTypeHeader: Headers.textPlainContentType,
-          },
-        ),
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: 'http://13.228.127.252:8114',
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: const {
+          Headers.contentTypeHeader: Headers.textPlainContentType,
+        },
       ),
-      permanent: true,
     );
 
-    Get.put<AuthRepository>(
-      AuthRepository(Get.find<Dio>()),
-      permanent: true,
-    );
+    Get.put<Dio>(dio, permanent: true);
 
-    final dio = Get.find<Dio>();
-    if (!dio.interceptors.any((interceptor) => interceptor is AuthTokenInterceptor)) {
-      final interceptor = AuthTokenInterceptor(
-        authRepository: Get.find<AuthRepository>(),
-        dio: dio,
-      );
-      dio.interceptors.add(interceptor);
-      Get.put<AuthTokenInterceptor>(interceptor, permanent: true);
-    }
+    final authRepository = AuthRepository(dio);
+    Get.put<AuthRepository>(authRepository, permanent: true);
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          if (options.extra['skip_auth'] == true) {
+            return handler.next(options);
+          }
+
+          final accessToken = await authRepository.getAccessToken();
+          if (accessToken != null && accessToken.isNotEmpty) {
+            options.headers["Authorization"] = 'Bearer $accessToken';
+          }
+
+          handler.next(options);
+        },
+        onError: (error, handler) async {
+          final shouldAttemptRefresh = error.response?.statusCode == 401 &&
+              error.requestOptions.extra['skip_auth'] != true &&
+              error.requestOptions.extra['retry_attempted'] != true;
+
+          if (!shouldAttemptRefresh) {
+            handler.next(error);
+            return;
+          }
+
+          try {
+            await authRepository.refreshSession();
+
+            final retryOptions = error.requestOptions;
+            retryOptions.extra['retry_attempted'] = true;
+
+            final response = await dio.fetch<dynamic>(retryOptions);
+            handler.resolve(response);
+          } catch (_) {
+            await authRepository.logout();
+            if (Get.currentRoute != Routes.login) {
+              Get.offAllNamed(Routes.login);
+            }
+            handler.next(error);
+          }
+        },
+      ),
+    );
   }
 }
-
