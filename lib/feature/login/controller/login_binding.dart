a33d9c7ba83c0070ee51e1c8ff6bcd56d67b2cb2
ahmad-mukhlish <ahmad.mukhlish.saputra@gmail.com>
2025-11-03 17:35:50 +0700

    init app scaffolding

---
+++ lib/feature/login/controller/login_binding.dart
@@ -0,0 +1,8 @@
+import 'package:get/get_instance/get_instance.dart';
+
+class LoginBinding extends Bindings {
+  @override
+  void dependencies() {
+
+  }
+}
---
+++ lib/services/network/api_service.dart
@@ -0,0 +1,77 @@
+import 'package:dio/dio.dart';
+import 'package:get/get.dart' hide Response;
+
+class ApiService extends GetxService {
+  ApiService({Dio? dio}) : _dio = dio ?? Dio(_defaultOptions);
+
+  final Dio _dio;
+
+  static BaseOptions get _defaultOptions {
+    return BaseOptions(
+      baseUrl: "",
+      connectTimeout: const Duration(seconds: 20),
+      receiveTimeout: const Duration(seconds: 20),
+      responseType: ResponseType.json,
+    );
+  }
+
+  Dio get client => _dio;
+
+  Future<ApiService> init() async {
+    return this;
+  }
+
+  Future<Response<T>> get<T>({
+    String path = '',
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.get<T>(
+      path,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> post<T>({
+    String path = '',
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.post<T>(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> put<T>({
+    String path = '',
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.put<T>(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+
+  Future<Response<T>> delete<T>({
+    String path = '',
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) {
+    return _dio.delete<T>(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+  }
+}
